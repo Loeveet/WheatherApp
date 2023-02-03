5fdfad83010a3b7faef8c02311effc35d4882f25
Robin <loeveet@gmail.com>
2023-02-03 13:26:37 +0100

    Started project

---
+++ WeatherApp/WeatherApp/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace WeatherApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            //
+        }
+    }
+}
